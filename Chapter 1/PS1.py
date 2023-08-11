b96a454227f3452c26b3c959686a258d4cc2b8c9
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-11 14:56:21 +0530

    :boom:  Learn About :- Modules, Comments and Pip  :boom:

---
+++ Chapter 1/PS1.py
@@ -0,0 +1,8 @@
+#  Print "Twinkle, twinkle, little star" Poem :-
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are''')
---
+++ Chapter 1/PS3.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\PRASHANT BHARDWAJ\\Downloads\\MahaKal Tandav ! Shiv Dhun.mp3') 
