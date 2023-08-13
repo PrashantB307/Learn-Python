44b5d44485c4f2f7b4fae7932ee3d1ec2150d2e4
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-13 15:06:17 +0530

    :boom:  Operators  :boom:

---
+++ Chapter 2/02_Operators.py
@@ -0,0 +1,12 @@
+
+a = 8
+b = 4
+
+# Arithmetic Operation
+print("The value of " ,a ," and ",b," are ",a+b)
+print("The value of " ,a ," and ",b," are ",a-b)
+print("The value of " ,a ," and ",b," are ",a*b)
+print("The value of " ,a ," and ",b," are ",a/b)
+
+
+
