cfaec50f603a2d7e6f960930eeeec1c81ca76cd7
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-13 15:05:41 +0530

    :boom:  Variables  :boom:

---
+++ Chapter 2/01_Variables.py
@@ -0,0 +1,11 @@
+a = "Prashant" 
+b = 234
+c = 23.5
+d = True
+e = None
+
+# Printing the variables
+print(a , b , c, d, e)
+
+# Printing the type of variables
+print(type(a), type(b), type(c), type(d), type(e))
