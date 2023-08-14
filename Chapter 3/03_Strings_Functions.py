77213b764a29c1825a230dc329a2649d2810ee6f
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-14 22:38:50 +0530

    :boom:  Strings_Functions  :boom:

---
+++ Chapter 3/03_Strings_Functions.py
@@ -0,0 +1,18 @@
+
+
+# String Functions ====>
+# ======================
+
+story = "Once upon a time there is a student name Prashant Bhardwaj, who studied in IES College of Technology"
+
+print(len(story))    # ==> 100
+
+print(story.endswith("Technology"))    # ==> True
+
+print(story.count("d"))    # ==> 4
+
+print(story.find("a"))    # ==> 10(index)
+
+print(story.replace("Prashant", "Pawan"))
+# Once upon a time there is a student name Pawan Bhardwaj, who studied in IES College of Technology
+
