9711844f51eabd9d5433c32d6ae36d1cac79e703
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-15 17:34:15 +0530

    :boom: Tuple  :boom:

---
+++ Chapter 4/04_Tuple.py
@@ -0,0 +1,16 @@
+
+
+# Create a Tuple using ()
+
+t = (3, 7 , 5 , 89 , 67 , 45 )
+print(t)
+# ==> (3, 7, 5, 89, 67, 45)
+
+
+# We can not update the value  of the tuple
+# t[3] = 33
+# print(t)    # ==> Error
+
+
+# Printing the value of the tuple
+print(t[4])   # ==> 67
