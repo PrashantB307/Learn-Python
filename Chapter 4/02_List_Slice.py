4be206356c67cf27854af7958e0eab3a991dcc7a
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-15 17:33:26 +0530

    :boom:  List - Slice  :boom:

---
+++ Chapter 4/02_List_Slice.py
@@ -0,0 +1,11 @@
+
+
+# List Slicing :===>
+# ==================
+
+friends = ["karan", 445 ,"pankaj", 34 ,"aalu", False , "bhalu", "kaalu"]
+
+print(friends[1:6])
+# ==> [445, 'pankaj', 34, 'aalu', False]
+
+print(friends[-4])   # ==> aalu
