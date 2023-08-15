6307ba7f278b65170746e75b4db1c2e68c576401
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-15 17:32:50 +0530

    :boom:  List  :boom:

---
+++ Chapter 4/01_List.py
@@ -0,0 +1,19 @@
+
+# Create LIst using []
+
+a = [1, 2, 4, 56, 6]
+print(a[4])      # ==> 6
+
+
+a[2] = 34   # change the value of list (index -2) 
+
+print(a)   # ==> [1, 2, 34, 56, 6]
+
+
+# We can create a list with items of diferent types
+
+list =[2, "pawan", "A", "Bhardwaj", 987 , False, 9.45]
+print(list) 
+# ==>  [2, 'pawan', 'A', 'Bhardwaj', 987, False, 9.45]
+
+
