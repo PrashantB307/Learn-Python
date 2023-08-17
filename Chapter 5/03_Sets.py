2d473bb6fbbf0cdfef6fb9f9ecc4a638722d1c6c
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-17 12:35:40 +0530

    :boom:    Sets   :boom:

---
+++ Chapter 5/03_Sets.py
@@ -0,0 +1,15 @@
+
+
+
+s = {1, 3, 5, 7, 1}
+
+print(type(s))   # ===> <class 'set'>
+
+print(s)    # ===> {1, 3, 5, 7} ( Egnor the repeated element)
+
+
+# Empty Set ====>
+# ===============
+
+b = set()
+print(type(b))   # ===> <class 'set'>
