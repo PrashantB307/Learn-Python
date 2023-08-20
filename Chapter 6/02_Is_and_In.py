f192afc5bf365b8462da6110b8725c9dcd9e56e8
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-20 14:47:00 +0530

    :boom:   Is_and_In   :boom:

---
+++ Chapter 6/02_Is_and_In.py
@@ -0,0 +1,21 @@
+ 
+
+
+#   Is and In =======> 
+# ====================
+
+a = None
+
+if(a is None):
+    print("Yes")
+else:
+    print("No")    
+
+
+
+
+arr = [20, 38, 45, 65, 67]
+
+print(56 in arr)     # =====> False
+
+print(45 in arr)     # =====> True
