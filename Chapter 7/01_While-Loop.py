a63ead7838536be64b100a27d58b619c764235c5
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-21 20:22:15 +0530

    :boom:   While_Loop   :boom:

---
+++ Chapter 7/01_While-Loop.py
@@ -0,0 +1,23 @@
+
+
+
+# Print 0 to 10 by While Loop =====>
+# ==================================
+
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1
+
+print("Finish")
+
+
+
+
+
+fruits = ["Banana", 'Apple', 'Mango', 'Guava', 'Grapes', 'Orange', 'Pine-Apple']
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i = i + 1
